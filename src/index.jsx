a1ba0c80abdfc79925ecf5e7f7624d60d4c53393
breiner2000 <breinergcp@estudiantec.cr>
2024-01-03 21:47:22 -0600

    fix some errors for eslint

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const root = document.getElementById('app')
+
+// Utilizando ReactDOM.createRoot
+const reactRoot = ReactDOM.createRoot(root)
+reactRoot.render(<App />)
