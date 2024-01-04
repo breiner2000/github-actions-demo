fb5ec933f123aa3d5d32f22420ce28c59cd4a528
breiner2000 <breinergcp@estudiantec.cr>
2024-01-03 21:54:54 -0600

    Fix lint errors

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const { version } = require('./package.json')
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+    res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send(version)
+    res.send(version)
 })
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
+    console.log(`server started on port ${PORT}`)
 })
